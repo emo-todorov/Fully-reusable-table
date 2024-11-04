ad9e532758ec03c6485b2044d098c1463d6a5616
emo-todorov <emiliyantodorov89@gmail.com>
2024-11-04 18:40:31 +0200

    Initial commit

--- src/components/SortingTable/SortingTable.jsx
+++ src/components/SortingTable/SortingTable.jsx
@@ -1,47 +1,12 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import CustomTable from '../CustomTable/CustomTable'
 import './sortingTable.css';
+import { useSort } from '../../customHooks/customHooks';
+import { ASCENDING, DESCENDING } from '../../constants/constants';
 
 const SortingTable = (props) => {
     const { data = [], config = [] } = props;
-    const [sortingOrder, setSortingOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
-
-    const sortData = useCallback((data = [], fieldName, sortingOrder) => {
-        const sortOrder = sortingOrder === 'asc' ? 1 : -1;
-
-        return data.sort((a, b) => {
-            const valueA = a[fieldName];
-            const valueB = b[fieldName];
-            let result;
-
-            if (typeof valueA === 'string') {
-                result = valueA.localeCompare(valueB) * sortOrder;
-            } else if (typeof valueA === 'number') {
-                result = (valueA - valueB) * sortOrder;
-            }
-
-            return result;
-        });
-    }, []);
-
-    const sortedData = sortData([...data], sortBy, sortingOrder);
-
-    const clickHandler = (field) => {
-        if (field !== sortBy) {
-            setSortingOrder('asc');
-            setSortBy(field);
-        } else if (sortingOrder === null) {
-            setSortingOrder('asc');
-            setSortBy(field);
-        } else if (sortingOrder === 'asc') {
-            setSortingOrder('desc');
-            setSortBy(field);
-        } else if (sortingOrder === 'desc') {
-            setSortingOrder(null);
-            setSortBy(null);
-        }
-    };
+    const { sortOrder, sortBy, sortedData, handleSort } = useSort(data);
 
     const getIcons = useMemo(() => (columnName) => {
         let icons = <>
@@ -50,22 +15,22 @@ const SortingTable = (props) => {
         </>;
 
         if (columnName === sortBy) {
-            if (sortingOrder === 'asc') {
-                icons = <div>🔼</div>
-            } else if (sortingOrder === 'desc') {
-                icons = <div>🔽</div>
+            if (sortOrder === ASCENDING) {
+                icons = <div>🔼</div>;
+            } else if (sortOrder === DESCENDING) {
+                icons = <div>🔽</div>;
             }
         }
 
         return icons;
-    }, [sortingOrder, sortBy]);
+    }, [sortOrder, sortBy]);
 
     const updatedConfig = config.map(colItem => {
         if (colItem.canSort) {
             return {
                 ...colItem,
                 renderColHeader(label) {
-                    return <div className='sort-cell-wrapper' onClick={() => clickHandler(colItem.field)}>
+                    return <div className='sort-cell-wrapper' onClick={() => handleSort(colItem.field)}>
                         <div>{label}</div>
                         <div>
                             {getIcons(colItem.field)}
@@ -79,9 +44,7 @@ const SortingTable = (props) => {
     });
 
     return (
-        <>
-            <CustomTable {...props} config={updatedConfig} data={sortedData} />
-        </>
+        <CustomTable {...props} config={updatedConfig} data={sortedData} />
     );
 };
 
